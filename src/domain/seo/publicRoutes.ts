c229cd11ab7f82708f72de97e86978349378c169
floriankreutzer <florian.kreutzer@me.com>
2026-08-29 11:09:48 +0200

    fix: keep SEO route registry test-runner independent

--- src/domain/seo/publicRoutes.ts
+++ src/domain/seo/publicRoutes.ts
@@ -1,5 +1,5 @@
-import { locales } from '@config/locales';
-import { publicPageSlugs } from '@i18n/publicPages';
+import { locales } from '../../config/locales';
+import { publicPageSlugs } from '../../i18n/publicPages';
 
 export function getIndexablePublicPaths(): string[] {
   return locales.flatMap((locale) => [
